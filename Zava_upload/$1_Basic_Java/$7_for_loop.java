64fd43b2d0ae3bbe4b7eb1450bec87d339a95ba1
MM1720 <121046061+MM1720@users.noreply.github.com>
2024-06-20 10:14:45 +0530

    Not meant for here

--- Zava_upload/$1_Basic_Java/$7_for_loop.java
+++
@@ -1,8 +0,0 @@
-package $1_Basic_Java;
-public class $7_for_loop {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Hello world");
-        }
-    }
-}
